369be8c1919b023b50e958822d3b2dd47932dfba
Mananhina <ee_gl@mail.ru>
2021-09-27 17:37:26 +0300

    Commit values comparing task

---
+++ task_907.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int w, h, r;
+    cin >> w >> h >> r;
+    if (min(w, h) >= r * 2) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
