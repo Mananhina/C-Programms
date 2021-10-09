42e84ef2b57d8eb7d73b54fe62c7de338740a2a3
Mananhina <ee_gl@mail.ru>
2021-10-10 00:35:46 +0300

    Add 2 week`s tasks

---
+++ homeworks/2 week/task_293.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int n, ans = 0, inc;
+  cin >> n;
+  vector<int> incomes(n);
+  vector<int> taxes(n);
+
+  for(int i = 0; i < n; ++i) cin >> incomes[i];
+  for(int i = 0; i < n; ++i) cin >> taxes[i];
+
+  inc = 0;
+
+  for(int i = 0; i < n; ++i){
+    if(taxes[i] * incomes[i] > inc){
+      inc = taxes[i] * incomes[i];
+      ans = i;
+    }
+  }
+  cout << ans + 1;
+}
---
+++ homeworks/2 week/task_637.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int n, k, ans = 0;
+  cin >> n;
+
+  vector<int> vec(n);
+  for(int i = 0; i < n; ++i) cin >> vec[i];
+
+  cin >> k;
+  for(int i = 0; i < vec.size(); ++i) ans += min(k, vec[i]);
+  cout << ans;
+}
